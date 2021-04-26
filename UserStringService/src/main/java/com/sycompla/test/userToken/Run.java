85d82244d0bba3cfe09e8fa08986a154187ad900
d7p4n4 <Szappan60>
2021-04-26 17:02:12 +0200

    1.1.20210426.7

---
+++ UserStringService/src/main/java/com/sycompla/test/userToken/Run.java
@@ -0,0 +1,99 @@
+package com.sycompla.test.userToken;
+
+import com.sycompla.entity.UserToken;
+import com.sycompla.object.userToken.*;
+import com.sycompla.stringService.user.UserStringService;
+
+public class Run {
+
+    public void getById(String request) {
+
+        //String request = new GetByIdRequest(2).getAsJson();
+
+        System.out.println(
+                new UserStringService().getById(
+                        request
+                )
+        );
+
+    } // getById
+
+
+    public void getByGuid(String request) {
+
+        String requestAsString = new GetByGuidRequest("tesztguid").getAsJson();
+
+        System.out.println(
+                new UserStringService().getById(
+                        request
+                )
+        );
+
+    } // getByGuid
+/*
+    public void insert() {
+
+        String request = new InsertRequest(
+                new User(
+                    "tesztG"
+                    , "tester János"
+                    , "test@mail.hu"
+                    , "pwd123"
+                    , "testtoken"
+                    , "magyar"
+                    , "+123456789"
+                    , "uName"
+            )
+        ).getAsJson();
+
+    } // insert
+
+    public void deleteByGuid(String request) {
+
+        String requestAsString = new DeleteByGuidRequest("tesztguid").getAsJson();
+
+        String response = new UserStringService().deleteByGuid(request);
+
+        System.out.println(response);
+
+    } // deleteByGuid
+
+    public void updateById(String request) {
+
+        String requestAsString = new UpdateByIdRequest(
+                new User(
+                        "tesztG"
+                        , "tester János"
+                        , "test@mail.hu"
+                        , "pwd123"
+                        , "testtoken"
+                        , "magyar"
+                        , "+123456789"
+                        , "uName"
+                )
+                , 6
+        ).getAsJson();
+
+        String response = new UserStringService().updateById(request);
+
+        System.out.println(response);
+
+    } // updateById
+*/
+    public static void main(String[] args) {
+
+        //new Run().getList();
+      //  new Run().getById("{'id': 2}");
+        new Run().getByGuid("");
+        //new Run().getById();
+        //new Run().isExistsById();
+        //new Run().isExistsByGuid();
+//        new Run().insert();
+//        new Run().updateById("{\"user\":{\"id\":0,\"guid\":\"null\",\"name\":\"tester János\",\"email\":\"test@mail.hu\",\"password\":\"pwd123\",\"oAuthToken\":\"testtoken\",\"language\":\"magyar\",\"phoneNumber\":\"+123456789\",\"userName\":\"uName\"},\"id\":10}");
+//        new Run().updateByGuid();
+//        new Run().delete();
+//        new Run().deleteByGuid("{\"guid\":\"tesztguid\"}");
+
+    } // main
+
+} // Run
