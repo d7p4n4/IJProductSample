06a09ae52704731d72eeab3aec599d8f2c7f85f5
d7p4n4 <Szappan60>
2021-04-27 16:03:07 +0200

    1.1.20210427.6

--- IJProductSampleCompositeService/src/main/java/com/sycompla/composite/CompositeService.java
+++ IJProductSampleCompositeService/src/main/java/com/sycompla/composite/CompositeService.java
@@ -1,9 +1,12 @@
 package com.sycompla.composite;
 
 import ac4y.service.domain.Ac4yProcessResult;
-import com.sycompla.object.userToken.IsExistsByFbTokenRequest;
-import com.sycompla.object.userToken.IsExistsByFbTokenResponse;
-import com.sycompla.object.userToken.UserTokenObjectService;
+import com.sycompla.object.user.GetByGuidRequest;
+import com.sycompla.object.user.GetByGuidResponse;
+import com.sycompla.object.user.IsExistsByGuidRequest;
+import com.sycompla.object.user.IsExistsByGuidResponse;
+import com.sycompla.object.user.UserObjectService;
+import com.sycompla.object.userToken.*;
 
 public class CompositeService {
 
@@ -55,5 +58,85 @@ public class CompositeService {
         return response;
 
     } // isUnknownOrInvalidToken
+
+    public GetUserFromByTokenResponse getUserFromByToken(GetUserFromByTokenRequest request) {
+
+        GetUserFromByTokenResponse response = new GetUserFromByTokenResponse();
+
+        try {
+
+            IsExistsByFbTokenResponse isExistsByFbTokenResponse = new UserTokenObjectService()
+                    .isExistsByFbToken(new IsExistsByFbTokenRequest(
+                            request.getFbToken()
+                    ));
+
+            if(isExistsByFbTokenResponse.getResult().getCode() == 1) {
+
+                GetByFbTokenResponse getByFbTokenResponse = new UserTokenObjectService()
+                        .getByFbToken(new GetByFbTokenRequest(
+                                request.getFbToken()
+                        ));
+
+                IsExistsByGuidResponse isExistsByGuidResponse = new UserObjectService()
+                        .isExistsByGuid(new IsExistsByGuidRequest(
+                                getByFbTokenResponse.getUserToken().getUserGuid()
+                        ));
+
+                if(isExistsByGuidResponse.getResult().getCode() == 1) {
+
+                    GetByGuidResponse getByGuidResponse = new UserObjectService()
+                            .getByGuid(new GetByGuidRequest(
+                                    getByFbTokenResponse.getUserToken().getUserGuid()
+                            ));
+
+                    response.setUser(getByGuidResponse.getUser());
+
+                    response.setResult(
+                            new Ac4yProcessResult(
+                                    1
+                                    , "ok"
+                                    , null
+                            )
+                    );
+
+                } else {
+
+                    response.setResult(
+                            new Ac4yProcessResult(
+                                    -1
+                                    , "nem létezik a guid-al user "
+                                    , null
+                            )
+                    );
+
+                }
+
+            } else {
+
+                response.setResult(
+                        new Ac4yProcessResult(
+                                -1
+                                , "létezik a token"
+                                , null
+                        )
+                );
+
+            }
+
+        } catch (Exception exception) {
+
+            response.setResult(
+                    new Ac4yProcessResult(
+                            -1
+                            , exception.getMessage()
+                            , null
+                    )
+            );
+
+        }
+
+        return response;
+
+    } // GetUserFromByToken
 
 } // CompositeService
